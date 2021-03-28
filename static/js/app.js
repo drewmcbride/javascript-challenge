932fedf6b426d4c0d971348f307ea4decf51ff6f
DrewMcB <drew.mcbride89@gmail.com>
2021-03-28 12:17:57 -0500

    Table displays properly on page

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,75 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Reference for the table body
+var tbody = d3.select("tbody");
+
+// Check data in console
+// console.log(tableData);
+
+// Iterate through data and display each row in the console
+// tableData.forEach(function(ufoReport) {
+//     console.log(ufoReport);
+//   });
+
+// Add table rows 
+// tableData.forEach(function(ufoReport) {
+//     console.log(ufoReport);
+//     var row = tbody.append("tr");
+//   });
+
+// 
+// tableData.forEach(function(ufoReport) {
+//   console.log(ufoReport);
+//   var row = tbody.append("tr");
+
+//   Object.entries(ufoReport).forEach(function([key, value]) {
+//     console.log(key, value);
+//     // Append a cell to the row for each value
+//     // in the weather report object
+//     var cell = row.append("td");
+//   });
+// });
+
+tableData.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runTablefilter);
+form.on("submit",runTablefilter);
+
+// Complete the event handler function for the form
+function runTablefilter() {
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
+
+// Get the value property of the input element
+var inputValue = inputElement.property("value");
+
+console.log(inputValue);
+console.log(tableData);
+
+var filteredData = tableData.filter(record => record.datetime === inputValue);
+
+console.log(filteredData);
+};
